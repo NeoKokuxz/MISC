e0202c40e6bc1bc16005d07a40d706ebf6b08586
Neo Chen <43288434+NeoKokuxz@users.noreply.github.com>
2020-04-27 13:42:16 -0400

    Update ALGO.java

--- ALGO.java
+++ ALGO.java
@@ -1,1 +1,27 @@
-
+//Question 2
+class BinaryTree { 
+    count = 0;
+    void Count(Node root, int key) { 
+          
+        /* base case */
+        while (root != null) { 
+            // pass right subtree as new tree 
+            if (key > root.data) 
+                root = root.right; 
+  
+            // pass left subtree as new tree 
+            else if (key < root.data) 
+                root = root.left; 
+          
+            else {
+                // if the key is found
+                //check left subtree
+                if(root.left != null){
+                  count++;
+                //check right subtree
+                } else if (root.right != null) {
+                  count++; 
+                }
+            }
+        } 
+    } 
